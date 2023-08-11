1681146b5f0d2cbf790554847b1ac53093221e62
Toru52 <torufukada@hotmail.com.tw>
2023-08-12 02:22:42 +0900

    added 10000>solved problems

---
+++ picoGym/Fresh Java(Java)/sort.py
@@ -0,0 +1,7 @@
+str="}d0a1efb2_d3r1uq3r_gn1l007{FTCocip"
+s_reversed_list = list(reversed(str))
+print(s_reversed_list)
+# ['e', 'd', 'c', 'b', 'a']
+
+s_reversed = ''.join(list(reversed(str)))
+print(s_reversed)
